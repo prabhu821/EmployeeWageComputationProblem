e5f1adbf1b0d6825ed332f64d62b896b55400d90
rishiraj007 <rishiraj@mounttalent.com>
2022-12-28 16:04:06 +0530

    [Prabhu] Add: Attendence

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
+            EmployeeWage employeeWage = new EmployeeWage();
             Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect the option");
+                Console.WriteLine("1.Attendance \n2.Exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        employeeWage.Attendence();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
